85f2021e18ce12595ff8447db43386f34d23b8d0
Mayur Prajapati <mproyur685@gmail.com>
2025-06-22 15:24:03 +0530

    room creation done

--- src/Context/SocketContext.tsx
+++ src/Context/SocketContext.tsx
@@ -1,5 +1,6 @@
 import SocketIoClient from "socket.io-client";
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const WS_Server = "http://localhost:5500"; // backend server
 
@@ -15,6 +16,17 @@ interface Props{
 }
 
 export const SocketProvider: React.FC<Props> = ({ children }) => {
+
+    const navigate = useNavigate(); // will help to programatically handle navigation
+    useEffect(() => {
+        const enterRoom = ({ roomId } : { roomId: string}) => {
+            navigate(`/room/${roomId}`);
+        }
+
+        socket.on("room-create", enterRoom);
+
+        
+    }, []);
     return (
         <SocketContext.Provider value={socket}>
             {children}
--- src/Pages/Room.tsx
+++ src/Pages/Room.tsx
@@ -1,7 +1,22 @@
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { SocketContext } from "../Context/SocketContext";
+
 const Room: React.FC = () => {
+
+    const { id } = useParams();
+    const socket = useContext(SocketContext);
+    
+    useEffect(() => {
+        //emitting this event so that eithe creator of room or joinee in the room anyone is added the server knows that new people have been added to the this room
+        if (socket && socket.connected) {
+            socket.emit("joined-room",{roomId: id});
+        } 
+        
+    },[]);
     return (
         <div>
-            Room
+            Room : {id};
         </div>
     )
 }
